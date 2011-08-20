1a84682b69ea86cfbb3b38022d7641fe8ef4a416
Gareth McMullin <gareth@blacksphere.co.nz>
2011-08-20 22:18:07 +1200

    Removed GtkEntry, use editable cell instead.
    Added icon.  Changed display of 'micro' suffix to greek mu.

--- vapor.py
+++ vapor.py
@@ -1,5 +1,6 @@
 import gtk
 import gtk.gdk
+import glib
 import rpn
 import math
 
@@ -14,18 +15,10 @@ def do_update():
 			sm.append((len(stacktop) - i, eng_formatter(stacktop[i])))
 		else:
 			sm.append((len(stacktop) - i, repr(stacktop[i])))
-	entry.set_text("")
-
-def do_activate(entry):
-	# Send text to RPN scanner
-	s = entry.get_text()
-	if not s: s = "dup"
-	rpn.dostr(s)
-	do_update()
 
 def eng_formatter(f, sigfigs=4):
 	# Format floats in engineering notation
-	mult = {-15: 'f', -12: 'p', -9: 'n', -6: 'u', -3: 'm', 
+	mult = {-15: 'f', -12: 'p', -9: 'n', -6: u'\u03bc', -3: 'm', 
 		0: '', 
 		3: 'k', 6: 'M', 9: 'G', 12: 'T'}
 
@@ -58,20 +51,20 @@ def eng_formatter(f, sigfigs=4):
 
 def do_keypress(entry, event):
 	keyname = gtk.gdk.keyval_name(event.keyval) 
-	if not entry.get_text():
-		if keyname == "BackSpace":
-			entry.set_text("drop")
-			do_activate(entry)
-			return True
+#	if not entry.get_text():
+#		entry.set_property("editing-canceled", True)
+#		entry.editing_done()
+#		entry.remove_widget()
+#		return True
 
-	if keyname == "Escape":
-		entry.set_text('')
-		return True
+#	if keyname == "Escape":
+#		return False
 		
 	if (keyname == "d") and (event.state & gtk.gdk.CONTROL_MASK):
+		entry.stop_editing(True)
 		#w.hide()
 		w.iconify()
-		#gtk.main_quit()
+		return True
 
 	keyop = {
 		"plus": '+', "KP_Add": '+',
@@ -84,15 +77,79 @@ def do_keypress(entry, event):
 	if keyop.has_key(keyname):
 		text = entry.get_text() + " " + keyop[keyname]
 		entry.set_text(text)
-		do_activate(entry)
+		entry.set_property("editing-canceled", False)
+		entry.editing_done()
+		entry.remove_widget()
 		return True
 	
 	return False
 
-move = False
-
 def button_press(ww, e):
 	w.window.begin_move_drag(int(e.button), int(e.x_root), int(e.y_root), int(e.time))
+	return True
+
+editing = False
+
+def do_changed(editable):
+	if not editable.get_text():
+		editable.set_property("editing-canceled", True)
+		editable.editing_done()
+		editable.remove_widget()
+
+def editing_started(cel, editable, path):
+	global editing
+	editable.connect("editing-done", editing_done)
+	editable.connect("key-press-event", do_keypress)
+	editable.connect("changed", do_changed)
+	editing = True
+
+def editing_done(editable):
+	global editing
+	if not editable.get_property("editing-canceled"):
+		s = editable.get_text()
+		rpn.dostr(s)
+	do_update()
+	sv.get_selection().unselect_all()
+	editing = False
+
+def key_press(ww, e):
+	if editing: return
+
+	keyname = gtk.gdk.keyval_name(e.keyval) 
+	print keyname
+	# Modifier keys to ignore.
+	mods = ["Control_L", "Control_R", 
+		"Shift_L", "Shift_R", 
+		"Escape", 
+		"Alt_L", "Alt_R",
+		"Meta_L", "Meta_R",
+		"Up", "Down", "Left", "Right",
+	]
+	if keyname in mods:
+		return True
+
+	if (keyname == "BackSpace") or (keyname == "Delete"):
+		rpn.dostr("drop")
+		do_update()
+		return True
+
+	if (keyname == "Return") or (keyname == "KP_Enter"):
+		rpn.dostr("dup")
+		do_update()
+		return True
+
+	if (keyname == "d") and (e.state & gtk.gdk.CONTROL_MASK):
+		#w.hide()
+		w.iconify()
+		return True
+
+	sm = ww.get_model()
+	it = sm.get_iter_first()
+	sm.remove(it)
+	path = sm.get_string_from_iter(sm.append())
+	ww.set_cursor(path, ww.get_column(1), True)
+	gtk.main_do_event(e.copy())
+	return True
 
 if __name__ == "__main__":
 	w = gtk.Window()
@@ -100,35 +157,44 @@ if __name__ == "__main__":
 	w.connect("destroy", gtk.main_quit)
 	w.set_default_size(200, 100)
 	w.set_title("VaporCalc")
+	w.set_icon_from_file("vapor-small.png")
 	w.set_decorated(False)
-	w.set_border_width(1)
+	w.set_border_width(2)
 	w.set_keep_above(1)
+	w.stick()
 	#w.set_skip_taskbar_hint(True)
 	w.set_skip_pager_hint(True)
 	w.set_opacity(0.9)
 	w.set_resizable(False)
+	# Put a background on the window
+	bgcolour = gtk.gdk.color_parse("black")
+	w.modify_bg(gtk.STATE_NORMAL, bgcolour)
 
 	vbox = gtk.VBox()
 	w.add(vbox)
 
 	sm = gtk.ListStore(str, str)
 	for i in range(4):
-		sm.append(('   ', None))
+		it = sm.append(('   ', None))
 	sv = gtk.TreeView(sm)
+	sv.set_cursor(sm.get_string_from_iter(it))
+	sv.set_size_request(150, -1)
 	sv.connect("button-press-event", button_press)
+	sv.connect("key-press-event", key_press)
 	sv.set_headers_visible(False)
 	sv.set_rules_hint(True)
 	sv.append_column(gtk.TreeViewColumn(None, gtk.CellRendererText(), text=0))
 	cel = gtk.CellRendererText()
 	cel.set_alignment(1, 0.5)
+	cel.set_property("editable", True)
+	cel.connect("editing-started", editing_started)
 	sv.append_column(gtk.TreeViewColumn(None, cel, text=1))
-	sv.set_can_focus(False)
+	sv.set_can_focus(True)
 	vbox.pack_start(sv, True, True)
 
-	entry = gtk.Entry()
-	entry.connect("activate", do_activate)
-	entry.connect("key-press-event", do_keypress)
-	vbox.pack_start(entry, False, False)
-
 	w.show_all()
+	#sv.grab_focus()
+	sv.get_selection().unselect_all()
+
 	gtk.main()
+
